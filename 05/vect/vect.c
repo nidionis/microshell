466d437d901d21d901e0b28c3b65632c62ea4b0a
Your Name <you@example.com>
2025-12-14 14:29:22 +0100

    operator ++ et --

--- 05/vect/vect.c
+++ 05/vect/vect.c
@@ -6,7 +6,7 @@
 /*   By: nidionis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 16:20:59 by nidionis          #+#    #+#             */
-/*   Updated: 2025/12/14 13:07:57 by nidionis         ###   ########.fr       */
+/*   Updated: 2025/12/14 14:29:00 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,106 @@
 #include "vect.h"
 
 vect::vect(int a, int b) {
-	this->a = a;
-	this->b = b;
+	this->_x = a;
+	this->_y = b;
 }
 
 vect::vect() {
 	*this = vect(0,0);
 }
 
-vect vect::operator+(const vect &y) {
+int vect::x() {
+	return _x;
+}
+
+int vect::y() {
+	return _y;
+}
+
+vect vect::operator+(const vect &y) const {
 	vect z;
-	z.a = a + y.a;
-	z.b = b + y.b;
+	z._x = _x + y._x;
+	z._y = _y + y._y;
 	return z;
 }
 
+vect vect::operator/(const vect &y) const {
+	vect z;
+	z._x = _x / y._x;
+	z._y = _y / y._y;
+	return z;
+}
+
+vect vect::operator-(const vect &y) const {
+	vect z;
+	z._x = _x - y._x;
+	z._y = _y - y._y;
+	return z;
+}
+
+vect vect::operator*(const vect &y) const {
+	vect z;
+	z._x = _x * y._x;
+	z._y = _y * y._y;
+	return z;
+}
+
+vect vect::operator*(const int i) const {
+	vect z;
+	z._x = _x * i;
+	z._y = _y * i;
+	return z;
+}
+
+vect vect::operator/(const int i) const {
+	vect z;
+	z._x = _x / i;
+	z._y = _y / i;
+	return z;
+}
+
+vect vect::operator+(const int i) const {
+	vect z;
+	z._x = _x + i;
+	z._y = _y + i;
+	return z;
+}
+
+vect vect::operator-(const int i) const {
+	vect z;
+	z._x = _x - i;
+	z._y = _y - i;
+	return z;
+}
+
+vect vect::operator-() const {
+	return vect(-_x, -_y);
+}
+
+vect vect::operator--() {
+	vect v = vect(_x - 1, _y - 1);
+	return v;
+}
+
+vect vect::operator++() {
+	vect v = vect(_x + 1, _y + 1);
+	return v;
+}
+
+vect &vect::operator--(int) {
+	this->_x--;
+	this->_y--;
+	return *this;
+}
+
+vect &vect::operator++(int) {
+	this->_x++;
+	this->_y++;
+	return *this;
+}
+
 std::ostream &operator<<(std::ostream &os, vect &x) {
-	os << x.a << " ";
-	os << x.b << "\n";
+	os << x.x() << " ";
+	os << x.y() << "\n";
 	return os;
 }
--- 05/vect/vect.h
+++ 05/vect/vect.h
@@ -1,11 +1,31 @@
 #include <iostream>
+
 class vect {
+	int _x;
+	int _y;
+
 	public:
-	int a;
-	int b;
 	vect(int a, int b);
+	vect(const vect& other);
 	vect();
 
-	vect operator+(const vect &x);
+	int x();
+	int y();
+
+	vect operator+(const vect &x) const;
+	vect operator+(int i) const;
+	vect operator-(const vect &x) const;
+	vect operator-(int i) const;
+	vect operator*(const vect &x) const;
+	vect operator*(int i) const;
+	vect operator/(const vect &x) const;
+	vect operator/(int i) const;
+
+	vect operator-() const;
+	vect operator++();
+	vect &operator++(int);
+	vect operator--();
+	vect &operator--(int);
 };
+
 std::ostream &operator<<(std::ostream &os, vect &x);
